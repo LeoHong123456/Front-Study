a37dde167b9e539db6719856678e810dafdd1b0d
leo001 <leo@gmail.com>
2022-12-26 22:59:09 +0800

    234

--- route-exercise/src/views/PlayingDetail.jsx
+++ route-exercise/src/views/PlayingDetail.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Navigate, NavLink, useNavigate, useSearchParams } from 'react-router-dom'
+import { NavLink, useNavigate, useSearchParams } from 'react-router-dom'
+import Loading from '../components/Loading';
 import './PlayingDetail.css'
 
 export default function PlayingDetail() {
@@ -12,94 +13,112 @@ export default function PlayingDetail() {
     headers: {
       "X-Client-Info": '{"a":"3000","ch":"1002","v":"5.2.1","e":"1671519450920948362444801"}',
       "X-Host": "mall.film-ticket.film.info"
-    }
+    },
+    timeout: 5000
   }
-  const [list, setList] = useState([]);
+  const [list, setList] = useState([])
+  const [loading, setLoading] = useState(true)
   useEffect(() => {
     axios.get(`/api/gateway?filmId=${id}&k=6941512`, config).then(res => {
       setList(res.data.data.film)
+      setLoading(false)
+    }).catch((error) => {
+      if (error.request) {
+        window.alert("网络异常，加载失败！")
+        setLoading(false)
+      }
     })
   }, [list]);
 
   const navigate = useNavigate();
-  function goBank(){
+  function goBank() {
     navigate(`/film`);
   }
 
   return (
-    <div className='playing-wrapper'>
-      <div className='playing-detail-dec'>
-        <div className='film-header'>
-          <div className='film-goback' onClick={()=>goBank()}></div>
-        </div>
-        <div className='playing-banner'>
-          <img className='playing-img' src={list?.poster} alt=''></img>
-        </div>
+    <>
+      {
+        loading ? (<Loading />) : (
+          <div className='playing-wrapper'>
+            <div className='playing-detail-dec'>
+              <div className='film-header'>
+                <div className='film-goback' onClick={() => goBank()}></div>
+              </div>
 
-        <div className='playing-detail-c'>
-          <div className='playing-grand-wrapper'>
-            <div><span>{list?.name}</span></div>
-            <div className='playing-grade'>
-              <span>{list?.grade}</span>
-              <span>分</span>
+              <div className='playing-banner'>
+                <img className='playing-img' src={list?.poster} alt=''></img>
+              </div>
+
+              <div className='playing-detail-c'>
+                <div className='playing-grand-wrapper'>
+                  <div><span>{list?.name}</span></div>
+                  <div className='playing-grade'>
+                    <span>{list?.grade}</span>
+                    <span>分</span>
+                  </div>
+                </div>
+
+                <div className='playing-content-title'>
+                  <span>{list?.category}</span>
+                  <span>{list?.premiereAt} 上映</span>
+                  <span>{list?.nation}</span>
+                </div>
+
+                <div className='playing-memo'>
+                  <p>{list?.synopsis}</p>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className='playing-content-title'>
-            <span>{list?.category}</span>
-            <span>{list?.premiereAt} 上映</span>
-            <span>{list?.nation}</span>
-          </div>
-          <div className='playing-memo'>
-            <p>{list?.synopsis}</p>
-          </div>
-        </div>
-      </div>
 
-      <div className='actors'>
-        <div className='actors-title-bar'>
-          <span className='actors-title-text'>演员列表</span>
-        </div>
-        <div className="actors-list">
-          <ul className='actors-item'>
-            {
-              list.actors?.map((item, index) => {
-                return (
-                  <li key={index}>
-                    <div className='actors-detail'>
-                      <img className='actors-img' src={item.avatarAddress} alt=""></img>
-                      <p>{item.name}</p>
-                      <p>{item.role}</p>
-                    </div>
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </div>
-      </div>
+            <div className='actors'>
+              <div className='actors-title-bar'>
+                <span className='actors-title-text'>演员列表</span>
+              </div>
 
-      <div className="photos">
-        <div className="photos-title-bar">
-          <p>剧照</p>
-          <p>全部({list.photos?.length})</p>
-        </div>
-        <div className="photos-list">
-          <ul className='row-scroll-items-nav'>
-            {
-              list.photos?.map((item, index) => {
-                return (
-                  <li key={index}>
-                    <img className='target-img' src={item} alt=""></img>
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </div>
-      </div>
-      <NavLink className="goSchedule">
-        <p>选座购票</p>
-      </NavLink>
-    </div>
+              <div className="actors-list">
+                <ul className='actors-item'>
+                  {
+                    list.actors?.map((item, index) => {
+                      return (
+                        <li key={index}>
+                          <div className='actors-detail'>
+                            <img className='actors-img' src={item.avatarAddress} alt=""></img>
+                            <p>{item.name}</p>
+                            <p>{item.role}</p>
+                          </div>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </div>
+            </div>
+
+            <div className="photos">
+              <div className="photos-title-bar">
+                <p>剧照</p>
+                <p>全部({list.photos?.length})</p>
+              </div>
+              <div className="photos-list">
+                <ul className='row-scroll-items-nav'>
+                  {
+                    list.photos?.map((item, index) => {
+                      return (
+                        <li key={index}>
+                          <img className='target-img' src={item} alt=""></img>
+                        </li>
+                      )
+                    })
+                  }
+                </ul>
+              </div>
+            </div>
+            <NavLink className="goSchedule">
+              <p>选座购票</p>
+            </NavLink>
+          </div>
+        )
+      }
+    </>
   )
 }
--- route-exercise/src/views/nowplaying/NowPlaying.jsx
+++ route-exercise/src/views/nowplaying/NowPlaying.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import Detail from '../Detail'
+import Loading from '../../components/Loading'
 import './NowPlaying.css'
 
 export default function NowPlaying() {
@@ -12,32 +13,53 @@ export default function NowPlaying() {
       "EagleEye-TraceID": "f4d67b3d1671523700537100044555",
       "X-Client-Info": '{"a":"3000","ch":"1002","v":"5.2.1","e":"1671519450920948362444801","bc":"440300"}',
       "X-Host": "mall.film-ticket.film.list"
-    }
+    },
+    timeout: 3000
   }
-
+  const [loading, setLoading] = useState(false);
   const [list, setList] = useState([])
   useEffect(() => {
+    setLoading(true);
     axios.get("api/gateway?cityId=440300&pageNum=1&pageSize=10&type=1&k=6223073", config)
-      .then(res => setList(res.data.data.films))
+      .then(res => {
+        setList(res.data.data.films)
+        setLoading(false)
+      })
+      .catch(err => {
+        console.log(err.request)
+        if (err.response) {
+          console.log(err.response.data)
+        }else if(err.request){
+          window.alert("请求超时！")
+          setLoading(false)
+        }
+      })
   }, []);
 
 
   const navigate = useNavigate();
-  function playingDetail(id){
+  function playingDetail(id) {
     navigate(`/playingDetail?id=${id}`)
   }
-  
+
   return (
-    <div className='video-container'>
-      <ul>
-        {
-          list.map((item, index) =>
-            <li key={index} onClick={()=>playingDetail(item.filmId)}>
-              <Detail {...item} />
-            </li>
-          )
-        }
-      </ul>
+    <div className='video-wrapper-temple'>
+      {loading ? (
+          <Loading/>
+      ):(
+        <div className='video-container'>
+        <ul>
+          {
+            list.map((item, index) => (
+              <li key={index} onClick={() => playingDetail(item.filmId)}>
+                <Detail {...item} />
+              </li>
+            ))
+          }
+        </ul>
+        </div>
+      )
+      }
     </div>
   )
 }
